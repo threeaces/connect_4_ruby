c15a2d9b1efcaa89e1899de3450d80477d1e67d9
threeaces <dev.threeaces@gmail.com>
2020-07-14 13:27:18 -0700

    delete some attributes

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,14 +1,7 @@
 class Cell
-  attr_reader :id, :adjacent_cells
   attr_accessor :data
 
-  def initialize(id)
-    @id = id
+  def initialize
     @data = nil
-    @adjacent_cells = []
-  end
-
-  def add_adjacent_cells(cells)
-    @adjacent_cells = cells
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,31 +1,9 @@
 require './lib/cell'
 
 describe Cell do
-  subject(:valid_cell) { described_class.new('test') }
+  subject(:valid_cell) { described_class.new }
 
-  context 'when ititialized with wrong number of arguemnt' do
-    it 'raises an error' do
-      expect { described_class.new }.to raise_error(ArgumentError)
-      expect { described_class.new('some', 'arg') }.to raise_error(ArgumentError)
-    end
-  end
-
-  context 'when initialized with one argument' do
-    it 'does not raise an error' do
-      expect { valid_cell }.not_to raise_error
-    end
-
-    it { is_expected.to have_attributes(id: 'test') }
-
+  context '#initialize' do
     it { is_expected.to have_attributes(data: nil) }
-
-    it { is_expected.to have_attributes(adjacent_cells: []) }
-  end
-
-  context '#add_adjacent_cells' do
-    it 'assign passed array to @adjacent_cells' do
-      valid_cell.add_adjacent_cells([1, 2, 3])
-      expect(valid_cell.adjacent_cells).to eq([1, 2, 3])
-    end
   end
 end
